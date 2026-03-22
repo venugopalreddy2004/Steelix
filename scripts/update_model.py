b74043d8864cdd31bd121e5ce153d2e69579c819
venugopalreddy2004 <raamigaani2004@gmail.com>
2026-03-22 22:21:08 +0530

    removing extra files

--- scripts/update_model.py
+++
@@ -1,6 +0,0 @@
-import onnx
-from onnx import shape_inference
-
-model = onnx.load("models/squeezenet1.1-7.onnx")
-inferred_model = shape_inference.infer_shapes(model)
-onnx.save(inferred_model, "models/squeezenet_thick.onnx")
